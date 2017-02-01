66860090995641701db0af658b029d6d50fe59bc
Irene Canuti <irene.canuti@gmail.com>
2017-02-01 16:06:09 +0000

    Implemented Up and Down to increase and decrease temperature

--- jasmine-standalone-2.5.2/spec/ThermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -6,8 +6,19 @@ describe('Thermostat', function() {
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-  
+
   it('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('increases temperature with an UP command', function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('decreses temperature with an DOWN command', function() {
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
 });
--- jasmine-standalone-2.5.2/src/Thermostat.js
+++ jasmine-standalone-2.5.2/src/Thermostat.js
@@ -3,3 +3,11 @@
 function Thermostat() {
   this.temperature = 20
 };
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function()  {
+  this.temperature -= 1;
+};
